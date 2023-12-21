6ebec37a850364e023b101ea667587391b0148fb
ArtemT12345 <153588244+ArtemT12345@users.noreply.github.com>
2023-12-21 14:54:35 +0300

    Add files via upload

---
+++ от1.pas
@@ -0,0 +1,38 @@
+﻿program zad1;
+const nmax=100;
+var a:array[1..nmax] of integer;
+    n,i,j,k,p:byte;
+begin
+repeat
+write('чисел в массиве');
+readln(n);
+until n in [1..nmax];
+writeln('Массив:');
+for i:=1 to n do
+ begin
+  a[i]:=random(10);
+  write(a[i]:3);
+ end;
+writeln;
+i:=1;
+while i<=n do
+ begin
+  write('Элемент ',a[i]:4);
+  j:=i+1;
+  k:=1;
+  while j<=n do
+   begin
+    if a[j]=a[i] then
+     begin
+      k:=k+1;
+      for p:=j to n-1 do
+      a[p]:=a[p+1];
+      n:=n-1;
+     end
+    else j:=j+1;
+   end;
+  writeln(k:3,' рз.');
+  i:=i+1;
+ end;
+readln
+end.
---
+++ от2.pas
@@ -0,0 +1,30 @@
+﻿program Project1;
+var
+  a,b: string; 
+  i,j: integer;
+  flag: boolean;
+  k: integer;
+begin
+  writeln('Введите основную строку: ');
+  Readln(a);
+  writeln('Введите подстроку: ');
+  Readln(b);
+  k:= 0;
+  for i:= 1 to length(a) do
+    begin
+      flag:= false;
+      for j:= 1 to length(b) do
+        if a[i-1+j] <> b[j] then
+          begin
+            flag:= true;
+            break;
+          end;
+      if not flag then
+        inc(k);
+    end;
+  if k <> 0 then
+    writeln('Количество вхождений подстроки в строку: ',k)
+  else
+    writeln('В основной строке не была найдена подстрока');
+  Readln;
+end.
